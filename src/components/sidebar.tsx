79664f9466386cb6a51ef2d6e635c2282d37f497
Þór Breki <89972078+boathungry@users.noreply.github.com>
2023-07-11 16:00:43 +0000

    feat: Started work on Progress Bar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import ProgressBar from './progressBar';
 import SidebarItem from './sidebarItem';
 import { sidebarLink } from './sidebarItem';
 import { useState } from 'react';
@@ -15,8 +16,11 @@ export default function Sidebar({ selected, items }: sidebarProps){
     const [selectedItem, setSelected] = useState(selected);
     return (
         <div className='flex flex-col h-screen w-64 bg-lm-whitesmoke-default'>
-            <div className='bg-lm-rose-default py-3 flex content-center justify-center'>
-                <h3 className='w-56 text-2xl text-lm-very-light drop-shadow-text shadow-lm-rose-very-dark flex text-center'>Stress Management</h3>
+            <div className='bg-lm-rose-default py-3 flex flex-col justify-center items-center content-center'>
+                <h3 className='w-56 text-2xl text-lm-very-light drop-shadow-text shadow-lm-rose-very-dark text-center'>Stress Management</h3>
+                <div className='w-full flex-row justify-center items-center'>
+                    <ProgressBar progress={0.69}></ProgressBar>
+                </div>
             </div>
             {/* Map items from props to actual SidebarItem components, using the item title as a key */}
             {items.map(item => (
