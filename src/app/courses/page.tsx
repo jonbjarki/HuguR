9f9d1584a6dba1f8d88a340b07c5abfafb4b1cd4
jonbjarki <jonbjarki02@gmail.com>
2023-07-11 16:01:41 +0000

    chore: added mock course data json file

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import styles from './courses.module.css'
 import Course from '@/components/course'
+import courses from '../../mockdata.json'
+
 
 export default function CourseHome() {
   return (
     <main className="mt-6 w-11/12 m-auto">
       <h1 className="text-4xl text-center font-normal">Courses</h1>
       <ul className="flex flex-row flex-wrap gap-8">
+        
         <Course
           ID={1}
           title="Stress Management"
