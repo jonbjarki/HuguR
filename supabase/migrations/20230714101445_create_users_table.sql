2ba3b6cc1d425d79b8af1d0c3b38ba5c16922ebc
PatrekurTH <patrekurth@gmail.com>
2023-07-14 10:50:28 +0000

    workflow should work

--- supabase/migrations/20230714101445_create_users_table.sql
+++
@@ -1,9 +0,0 @@
--- Create a table called "users"
-create table
-  users (
-    -- Create a column called "id" that is an auto-incrementing integer and is the primary key
-    id integer primary key generated always as identity,
-    name text,
-    email text,
-    created_at timestamptz default now()
-  );
