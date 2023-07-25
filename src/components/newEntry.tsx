d95e5b27a8aa0e6ef05cd62e628784ab76bf5b90
Þór Breki <89972078+boathungry@users.noreply.github.com>
2023-07-25 14:37:29 +0000

    chore: small layout changes

--- src/components/newEntry.tsx
+++ src/components/newEntry.tsx
@@ -9,8 +9,9 @@ export interface newEntryProps {
 
 export function NewEntry({title, children, next, prev}: newEntryProps) {
     return (
-        <div className="border border-base-content rounded-md p-4 m-6">
+        <div className="border border-base-content rounded-md p-4 m-8">
             <div className="relative w-full">
+                {/* Close/cancel button */}
                 <button className="btn btn-primary btn-circle btn-outline btn-sm border-2 absolute top-0 right-0">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
@@ -18,7 +19,7 @@ export function NewEntry({title, children, next, prev}: newEntryProps) {
                 </button>
                 <h1 className="font-thin tracking-widest text-2xl text-center">{title}</h1>
             </div>
-            <div className="m-4">{children}</div>
+            <div className="m-4 text-center">{children}</div>
         </div>
     )
 }
