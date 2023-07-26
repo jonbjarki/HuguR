a85f72004d7f5164e00d3047ce4b1c94cbeef6ec
Þór Breki <thorthorgrims@gmail.com>
2023-07-26 14:06:41 +0000

    feat: clicking new entry button brings up new entry dialogue

--- src/app/courses/[id]/toolbox/page.tsx
+++ src/app/courses/[id]/toolbox/page.tsx
@@ -1,34 +1,20 @@
-import { EntryFrame } from "@/components/entryFrame";
 import { NewEntry } from "@/components/newEntry";
 import { Tool } from "@/components/tool";
+import { ToolsList } from "@/components/toolsList";
+import { useState } from "react"
 
 export default function CourseToolbox() {
-    let day = new Date(); // Placeholder
 
-    // TODO: fetch descriptions + dates + titles from database
+    // TODO: fetch tools from database
     return (
         <div className="w-full flex flex-col items-center">
             <h1 className="text-4xl text-base-content opacity-60 py-8">Toolbox</h1>
-            <p className="text-base-content opacity-60 pb-8 w-1/2">At the end of each module, you can write down something helpful you&apos;ve learned. This is where those “tools” will appear.
-You can also add new tools at any time.</p>
-            <div className="w-2/3 flex flex-col pb-8">
-                <Tool 
-                title="Don't be sad" 
-                description="Just don't do it. Why would you be sad? Idiot" 
-                date={day.toLocaleDateString()} />
-                <Tool 
-                title="Don't be sad" 
-                description="Just don't do it. Why would you be sad? Idiot" 
-                date={day.toLocaleDateString()} />
-                <Tool 
-                title="Don't be sad" 
-                description="Just don't do it. Why would you be sad? Idiot" 
-                date={day.toLocaleDateString()} />
-                <button className="btn btn-primary flex self-center m-4">New tool</button>
-                <NewEntry title="title">
-                    bla
-                </NewEntry>
-            </div>
+            <p className="text-base-content opacity-60 pb-8 w-1/2">
+                At the end of each module, you can write down something helpful you&apos;ve learned. 
+                This is where those “tools” will appear.
+                You can also add new tools at any time.
+            </p>
+            <ToolsList tools={[<Tool key={0} title="Don't be sad" description="Just don't do it. It's that easy!" date={"26/7/2023"} />]} />
         </div>
     )
 }
---
+++ src/components/toolsList.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { ReactNode, useState } from "react"
+import { Tool } from "./tool"
+import { NewEntry } from "./newEntry";
+
+export function ToolsList({tools=[]}: {tools: Array<ReactNode>}) {
+
+    let day = new Date(); // Placeholder
+
+    const [toolsList, setToolsList] = useState(tools);
+    const [addingEntry, setAddingEntry] = useState(true);
+
+    function addToolClicked() {
+        // setToolsList([toolsList.concat(<Tool title="Don't be sad" description="Just don't do it. It's that easy!" date={day.toLocaleDateString()} />)])
+        setAddingEntry(true);
+    }
+
+    function handleClose() {
+        setAddingEntry(false);
+    }
+
+    return (
+        <div className="w-2/3 flex flex-col pb-8">
+                {toolsList}
+                <button 
+                    onClick={addToolClicked} 
+                    className={"btn btn-primary flex self-center m-4 " + (addingEntry ? "hidden" : "visible")}
+                >
+                    New tool
+                </button>
+                <div className={addingEntry ? "visible" : "hidden"}>
+                    <NewEntry title="New Toolbox Entry" onClose={handleClose}>
+                        Pick a title for your entry
+                    </NewEntry>
+                </div>
+            </div>
+    )
+}
