050ee7df48bbfeb03e357d43433e4ef1ff367ea4
jonbjarki <jonbjarki02@gmail.com>
2023-07-27 10:21:01 +0000

    work on article and reading pages

--- src/components/ArticleSidebar.tsx
+++ src/components/ArticleSidebar.tsx
@@ -6,6 +6,11 @@ export default async function ArticleSidebar() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data, error } = await supabase.from('articles').select('title, id');
 
+  if (error) {
+    console.log(error);
+    return;
+  }
+
   return (
     <aside className="sticky w-60 top-0 left-0 -translate-x-full transform bg-white transition-transform duration-150 ease-in md:translate-x-0 md:shadow-md">
       <div className="w-full border-b-4 pb-7 mt-6 h-12 border-indigo-100 text-center">
