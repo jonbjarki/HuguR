6567118493ff289aa712e4b233ee0faa26e7c30a
jonbjarki <jonbjarki02@gmail.com>
2023-08-08 10:27:54 +0000

    fixed header merge conflict

--- src/components/navbar/header.tsx
+++ src/components/navbar/header.tsx
@@ -16,43 +16,9 @@ export default async function Header() {
   } = await supabase.auth.getUser();
 
   return (
-<<<<<<< HEAD
-    <header className="sticky h-16 bg-lm-very-light top-0 z-[99] flex flex-row flex-nowrap justify-between items-center shadow-md md:px-4">
-      <HomeIcon />
-
-      <nav className="hidden md:flex flex-row flex-nowrap text-lg gap-6">
-        <Link href="/reading" className="">
-          Reading
-        </Link>
-        <Link href="/courses" className="" prefetch>
-          Courses
-        </Link>
-        <Link href="/" className="">
-          Exercises
-        </Link>
-        <Link href="/diary" className="">
-          Diary
-        </Link>
-      </nav>
-
-      <h1 className="text-3xl md:hidden">HuguR</h1>
-
-      <menu className="hidden md:flex flex-row flex-nowrap gap-6">
-        <li>
-          <button className="">Get Support</button>
-        </li>
-        <li>
-          <button className="">EN v</button>
-        </li>
-      </menu>
-
-      <div className="hidden md:block">
-        {user ? <Avatar /> : <LoginButton />}
-=======
     <header className="navbar sticky bg-lm-very-light z-[99] shadow-md top-0 justify-evenly">
       <div className="navbar-start flex-shrink">
         <HomeIcon />
->>>>>>> 20d08bb392992db894bf242a1ab0a341d7dd1ea1
       </div>
       <h1 className="text-3xl md:hidden">HuguR</h1>
       <div className="hidden md:block navbar-center">
