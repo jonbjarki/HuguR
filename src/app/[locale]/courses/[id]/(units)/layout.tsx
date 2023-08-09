89ff136b652888cdc81df5c26fe1a27ea982428e
Þór Breki <89972078+boathungry@users.noreply.github.com>
2023-08-09 10:43:35 +0000

    feat: route groups for different sidebars

---
+++ src/app/[locale]/courses/[id]/(units)/layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Sidebar, { sidebarLink } from '@/components/sidebar/sidebar';
+import Icon from '@mdi/react';
+import { mdiChevronLeft } from '@mdi/js';
+import Link from 'next-intl/link';
+import { useParams } from 'next/navigation';
+
+export default function UnitLayout({ children }) {
+  const params = useParams();
+  const id = params != null ? params.id : '0'; // default to id=0 if params are null
+  const unit = params != null ? params.unit : '0'; // default to unit=0 if params are null
+  let sidebarItems = Array<sidebarLink>(
+    { title: 'Unit 1', link: '/courses/' + id + '/1', finished: true },
+    { title: 'Unit 2', link: '/courses/' + id + '/2', finished: true },
+    { title: 'Unit 3', link: '/courses/' + id + '/3', finished: false },
+  );
+  return (
+    <div className="flex flex-row h-auto min-h-screen">
+      <div className="w-1/5 flex">
+        <Link
+          href={'/courses/' + id + '/content'}
+          className="absolute z-10 top-2 left-1 h-10 w-10 text-base-100 drop-shadow-text"
+        >
+          <Icon path={mdiChevronLeft} />
+        </Link>
+        {/* TODO: Get week from database */}
+        <Sidebar
+          title="Week 1"
+          selected={'Unit ' + unit}
+          items={sidebarItems}
+        ></Sidebar>
+      </div>
+      <div className="w-4/5 flex bg-base-100">{children}</div>
+    </div>
+  );
+}
--- src/components/sidebar/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem.tsx
@@ -1,43 +1,58 @@
 'use client';
+import Icon from '@mdi/react';
+import { mdiCircleOutline, mdiCheckCircle } from '@mdi/js';
 import Link from 'next-intl/link';
 
-// The title of the link in the sidebar, and where it leads to.
-export interface sidebarLink {
+export interface SidebarItemProps {
   title: string;
   link: string;
-}
-
-export interface SidebarItemProps {
-  link: sidebarLink;
   isSelected?: boolean;
   setSelected?: any;
+  finished?: boolean | undefined;
 }
 
 export default function SidebarItem({
+  title,
   link,
   isSelected = false,
   setSelected,
+  finished,
 }: SidebarItemProps) {
-  const selectedSettings = isSelected ? 'bg-lm-whitesmoke-dark font-bold' : '';
+  let style =
+    'py-3 hover:bg-secondary-focus flex flex-row text-lg text-secondary-content';
+  const selectedSettings = isSelected ? ' bg-lm-whitesmoke-dark font-bold' : '';
+  style += selectedSettings;
 
   // Highlight selected item
   function handleClick() {
     if (setSelected != undefined) {
-      setSelected(link.title);
+      setSelected(title);
+    }
+  }
+
+  function showFinished() {
+    if (finished != undefined) {
+      if (finished)
+        return (
+          <Icon path={mdiCheckCircle} className="w-8 h-8 text-primary-focus" />
+        );
+      else
+        return (
+          <Icon
+            path={mdiCircleOutline}
+            className="w-8 h-8 text-primary-focus"
+          />
+        );
     }
   }
 
   return (
-    <Link
-      href={link.link}
-      onClick={handleClick}
-      className={
-        'py-3 hover:bg-lm-light flex flex-row text-lg text-lm-dark ' +
-        selectedSettings
-      }
-    >
-      <div className="w-7">{isSelected ? '—' : ''}</div>
-      <h1>{link.title}</h1>
+    <Link href={link} onClick={handleClick} className={style}>
+      <div className="flex flex-row items-center">
+        <div className="w-7">{isSelected ? '—' : ''}</div>
+        <div className="px-2">{showFinished()}</div>
+        <h1>{title}</h1>
+      </div>
     </Link>
   );
 }
