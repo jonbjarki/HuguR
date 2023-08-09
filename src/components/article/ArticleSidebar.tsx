accc9cf7468bc7244c283230e148fdcf98e1dbb3
Jón Bjarki <124071117+jonbjarki@users.noreply.github.com>
2023-08-09 14:00:07 +0000

    chore: removed redundant code

--- src/components/article/ArticleSidebar.tsx
+++ src/components/article/ArticleSidebar.tsx
@@ -25,7 +25,6 @@ export default async function ArticleSidebar() {
           <Link
             key={article.id}
             className="w-full h-full min-h-16 pl-4 text-lg flex items-center  hover:bg-lm-light"
-            prefetch
             href={`/reading/${article.id}`}
           >
             {article.title}
