39c4b91ed5210a81ffddcea5e56d8c47936686de
Þór Breki <89972078+boathungry@users.noreply.github.com>
2023-11-18 17:07:28 +0000

    chore: fixed some syntax errors in seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -31,3 +31,95 @@ INSERT INTO units (module_id, name, task) VALUES
 (1, '{"is": "Streita í háskólanámi", "en": "Stress in Higher Education"}', NULL),
 (1, '{"is": "Verkfærakistan - verkfærin", "en": "Toolbox - The Tools"}', NULL),
 (1, '{"is": "Varnagli", "en": "Precaution"}', NULL);
+
+INSERT INTO articles (title, content,description,image) VALUES (
+    'Getting Started',
+    "$$# Unlocking Inner Resilience: A Guide to Initiating Your Mental Health Journey
+*Your mental health journey begins with a single step—a decision to prioritize your well-being and embrace a path of self-improvement. In this guide, we'll explore practical advice and time-tested strategies to help you kickstart your quest for improved mental health.*
+## **1. Acknowledge and Accept Your Feelings**
+*The first and most crucial step towards better mental health is acknowledging and accepting your feelings. It's normal to experience a wide range of emotions, and suppressing them can be detrimental to your well-being. Embrace your emotions, both positive and negative, and allow yourself to process them without judgment.*
+## **2. Seek Support and Connection**
+*Remember, you don't have to go through this journey alone. Reach out to friends, family members, or seek support from mental health professionals. Talking about your feelings and experiences with someone you trust can be incredibly therapeutic and help you gain new perspectives.*
+## **3. Practice Mindfulness and Meditation**
+*Mindfulness and meditation can be powerful tools in calming your mind and reducing stress. Take time each day to engage in mindfulness practices, such as deep breathing exercises or meditation. These practices can help you stay present and cultivate a sense of inner peace.*
+## **4. Prioritize Self-Care**
+*Self-care is not a luxury; it's a necessity for maintaining good mental health. Make time for activities that bring you joy and relaxation, whether it's reading a book, spending time in nature, or pursuing a hobby. Taking care of yourself will recharge your mind and improve your overall well-being.*
+## **5. Set Realistic Goals**
+*Start small and set achievable goals for yourself. Whether it's getting enough sleep, eating healthier meals, or engaging in regular physical activity, incremental changes can have a significant impact on your mental health. Celebrate your achievements, no matter how small they may seem.*
+## **6. Challenge Negative Thought Patterns**
+*Negative thought patterns can sabotage your mental health progress. Learn to recognize these patterns and challenge them. Replace negative thoughts with more positive and empowering ones. Cognitive-behavioral techniques can be helpful in breaking free from harmful thinking habits.*
+## **7. Limit Exposure to Stress**
+*Identify sources of stress in your life and take steps to reduce or manage them. Sometimes, it might mean setting boundaries, saying no to additional responsibilities, or seeking professional help to navigate through challenging situations.*
+## **8. Practice Gratitude**
+*Cultivate an attitude of gratitude by regularly acknowledging the things you are thankful for. Gratitude can shift your focus from what's lacking to what's already present in your life, fostering a sense of contentment and happiness.*
+## **9. Stay Active and Engaged**
+*Physical activity is not only beneficial for your body but also for your mind. Engage in regular exercise to release endorphins, which are natural mood boosters. Additionally, staying socially engaged and connecting with others can combat feelings of loneliness and isolation.*
+## **10. Be Patient with Yourself**
+*Improving your mental health is a journey that takes time and effort. Be patient with yourself and don't expect immediate results. Allow yourself to embrace setbacks as learning opportunities and continue moving forward with compassion and determination.*
+Remember, seeking professional help is never a sign of weakness; it's a sign of strength and self-awareness. If you find that your mental health journey is overwhelming or too challenging to navigate on your own, consider reaching out to a mental health professional who can provide guidance and support along the way.
+*Take these steps to heart, and you'll find yourself on a path towards greater mental well-being and a more fulfilling life.*$$,
+$$This article offers essential insights and actionable tips for embarking on a transformative path towards mental well-being. From nurturing self-compassion to cultivating healthy habits, discover the key strategies to empower yourself and lay a solid foundation for a more balanced and fulfilling life.$$",
+'/images/peopletalking.jpg'
+),
+('Embracing Vulnerability', "$$## **1. The Strength in Vulnerability**
+*Vulnerability is often misunderstood as a weakness, but in reality, it is a profound display of courage and strength. Acknowledging and embracing vulnerability can be an essential catalyst for positive change in your mental health. It involves accepting imperfections, acknowledging past traumas, and recognizing the need for support.*
+## **2. Breaking Down the Walls**
+*Many of us build emotional walls to protect ourselves from pain, rejection, or judgment. However, these barriers can hinder personal growth and limit our ability to form meaningful connections. Learn how to identify these walls and take gradual steps to break them down, allowing yourself to experience vulnerability without fear.*
+## **3. Self-Compassion: A Pillar of Vulnerability**
+*Practicing self-compassion is fundamental in cultivating vulnerability. Treat yourself with the same kindness and understanding you would offer a close friend. Embrace your imperfections and mistakes as part of being human, fostering a nurturing environment for self-growth and emotional healing.*
+## **4. The Healing Power of Sharing**
+*Sharing your thoughts and emotions with trusted individuals can be a liberating experience. Find safe spaces to express your feelings, whether it's through journaling, art, or open conversations with empathetic friends or professionals. Witness the transformative power of vulnerability as you unburden yourself of pent-up emotions.*
+## **5. Vulnerability in Relationships**
+*True intimacy in relationships can only be achieved through vulnerability. Open up to your loved ones about your feelings, fears, and desires, creating a deeper emotional connection. Trust and vulnerability go hand in hand, and as you let your guard down, you may find stronger, more authentic bonds forming.*
+## **6. Embracing Change and Uncertainty**
+*Embracing vulnerability involves stepping into the unknown and being open to change. Understand that vulnerability might evoke discomfort, but it also brings growth and resilience. Embrace the uncertainties with an open heart, knowing that they can lead to new opportunities for personal development.*
+## **7. Seeking Professional Support**
+*Sometimes, navigating vulnerability can be challenging on your own. Seeking guidance from a mental health professional can provide valuable insights and tools to cope with emotional challenges. A therapist or counselor can offer a supportive and non-judgmental space to explore vulnerability and its impact on your mental well-being.*
+## **8. Cultivating Emotional Resilience**
+*Vulnerability and emotional resilience are closely linked. As you learn to navigate vulnerable moments, you develop resilience to bounce back from setbacks and adversity. Embrace vulnerability as a valuable teacher in life's journey, empowering you to face challenges with newfound strength.*
+## **9. Celebrating Vulnerability as Liberation**
+*Instead of fearing vulnerability, start celebrating it as a path to liberation. Embrace the freedom of being your authentic self, without the constraints of hiding behind masks or facades. Embracing vulnerability can lead to a more fulfilling life where you can connect with others on a deeper level.*
+## **10. Embracing Vulnerability as a Lifelong Journey**
+*Lastly, remember that embracing vulnerability is not a one-time event but a lifelong journey. Be patient and kind with yourself, as vulnerability may take time to fully integrate into your life. Embrace each step of this transformative process, knowing that you are embarking on a powerful quest to improve your mental health and emotional well-being.*
+**Conclusion:**
+*Incorporating vulnerability into your mental health journey can lead to remarkable personal growth and emotional healing. Embrace the power of vulnerability, recognize its strength, and discover the profound impact it can have on your overall well-being. Through openness and self-compassion, you can foster authentic connections with others and cultivate emotional resilience, leading to a more fulfilling and balanced life.*$$,
+$$In this thought-provoking article, we delve into the significance of vulnerability in the context of mental health. Discover how embracing vulnerability can lead to healing, growth, and authentic connections. Uncover practical ways to open up, both to yourself and others, as you embark on a transformative journey towards emotional well-being.$$",
+'/images/diarypic.png');
+    
+/* DIARY SEED */
+INSERT INTO diary(id, mood, thoughts,behaviour,coping_strategies,reassessment,circumstance,date,user_id)
+VALUES (1, 3, 'I was mad at myself for not remembering how to center a div', 'I ran out of the room crying', 'I looked up how to center a div', 'I now know how to center a div', 'Programming','2023-08-02','f50ab6cd-0a2e-4f61-b994-78fb295a4bc2');
+INSERT INTO emotions (name, intensity, diary_id) VALUES ('Sadness', 4, 1);
+INSERT INTO emotions (name, intensity, diary_id) VALUES ('Anger', 2, 1);
+INSERT INTO symptoms (name, diary_id) VALUES ('Crying', 1);
+INSERT INTO "public"."courses" (name, duration, description, image_url) VALUES (
+    'Stress Management', 6, 'Learn effective strategies to manage stress and anxiety, including mindfulness techniques and stress-reduction exercises.', 'personsofa.png'
+), (
+    'Building Resilience', 4, 'Discover techniques to enhance your resilience and develop coping mechanisms to deal with life''s challenges.', 'personsofa.png'
+), (
+    'Positive Psychology: The Science of Happiness', 8, 'Explore the principles of positive psychology and learn evidence-based strategies to cultivate happiness and well-being in your life.', 'personsofa.png'
+);
+INSERT INTO "public"."units" (number, content, week, course_id, task)
+VALUES
+    (1, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 1, 1, 'write your thoughts'),
+    (2, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 1, 1, 'write your thoughts'),
+    (3, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 1, 1, 'write your thoughts'),
+    (4, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 1, 1, 'write your thoughts'),
+    (1, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 2, 1, 'write your thoughts'),
+    (2, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 2, 1, 'write your thoughts'),
+    (3, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 2, 1, 'write your thoughts'),
+    (4, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 2, 1, 'write your thoughts'),
+    (1, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 3, 1, 'write your thoughts'),
+    (2, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 3, 1, 'write your thoughts'),
+    (3, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 3, 1, 'write your thoughts'),
+    (4, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 3, 1, 'write your thoughts'),
+    (1, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 4, 1, 'write your thoughts'),
+    (2, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 4, 1, 'write your thoughts'),
+    (3, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 4, 1, 'write your thoughts'),
+    (4, '[{"title": "some title", "content": "some cotent"}, {"title": "some other title", "content": "some more content"}]', 4, 1, 'write your thoughts');
+-- Emotions Options
+insert into emotions_options (name) values
+('Anger'), ('Anxiety'), ('Fear'), ('Sadness'), ('Stress');
+-- Symptoms Options
+insert into symptoms_options (name) values
+('Sweating'), ('Hyperventilating'), ('Tremors'), ('Crying'), ('Headache');
