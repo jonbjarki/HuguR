e98ebbec783dfe80fba856865992448d78c4ece0
Þór Breki <89972078+boathungry@users.noreply.github.com>
2023-12-07 14:13:13 +0000

    Revert "hotfix: temporarily disabled sidebar database fetch and changed mdx styling"

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,19 +5,16 @@ import type { MDXComponents } from 'mdx/types';
 // React component you want, including components from
 // other libraries.
 
-const br = () => <br />;
-
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    br,
-    h1: (props) => <h1 className="text-3xl font-extrabold" {...props} />,
-    h2: (props) => <h2 className="text-2xl font-bold" {...props} />,
-    h3: (props) => <h3 className="text-xl font-bold" {...props} />,
-    p: (props) => <p className="text-lg" {...props} />,
+    h1: (props) => <h1 className="text-2xl font-extrabold" {...props} />,
+    h2: (props) => <h2 className="text-xl font-bold" {...props} />,
+    h3: (props) => <h3 className="text-lg font-bold" {...props} />,
+    p: (props) => <p className="text-base" {...props} />,
     a: (props) => <a className="italic underline" {...props} />,
-    ul: (props) => <ul className="list-disc list-inside ul" {...props} />,
-    ol: (props) => <ol className="list-decimal list-inside ol" {...props} />,
+    ul: (props) => <ul className="list-disc list-inside" {...props} />,
+    ol: (props) => <ol className="list-disc list-inside" {...props} />,
     li: (props) => <li className="text-base" {...props} />,
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
