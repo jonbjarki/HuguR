ce4a47f0b45abf2b102ef302c6973f25ec49087f
jonbjarki <jonbjarki02@gmail.com>
2023-12-17 21:11:06 +0000

    minor styling and content changes

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,5 @@
 import type { MDXComponents } from 'mdx/types';
+import Image from 'next/image';
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
@@ -19,7 +20,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ),
     h2: (props) => <h2 className="text-3xl font-bold mt-6 mb-4" {...props} />,
     h3: (props) => <h3 className="text-2xl font-bold mt-6" {...props} />,
-    p: (props) => <p className="text-lg my-1 font-serif" {...props} />,
+    p: (props) => <p className="text-lg my-1 font-serif flex-col md:flex-row flex justify-start items-center" {...props} />,
     a: (props) => <a className="italic underline" {...props} />,
     ul: (props) => (
       <ul className="list-disc list-inside ul marker:text-primary" {...props} />
@@ -32,6 +33,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {...props}
       />
     ),
+    img: (props) => <img className='aspect-auto w-[100%] md:w-[50%]' {...props} />,
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
