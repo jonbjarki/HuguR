2fa6761dcb86fb7c4a4dc8955dffc88590961185
jonbjarki <jonbjarki02@gmail.com>
2024-01-05 12:25:54 +0000

    finished basic version of week 2 of stress management

--- src/components/unit/TextareaInput.tsx
+++ src/components/unit/TextareaInput.tsx
@@ -1,8 +1,15 @@
-export default function TextareaInput({ title }: { title: string }) {
+/**
+  Component that returns a text area input field with a title and a divider if title is provided.
+  @param {string} title - Title of the text area input field.
+ */
+
+export default function TextareaInput({ title }: { title?: string }) {
   return (
     <div className="flex flex-col gap-8 mt-8 w-full place-items-center">
+      { title ?
       <div className="divider mx-auto w-4/5">
-        {title}</div>
+        {title}</div> 
+         : null }
       <textarea
         placeholder="Type here..."
         className="textarea textarea-bordered border-primary textarea-md w-full max-w-md"
